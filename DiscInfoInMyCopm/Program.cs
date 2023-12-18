9eb830ef2af53cc562aca0df5390b8386d980075
tyr1kqq <igorturkinwhite@yandex.ru>
2023-12-18 14:31:14 +0200

    DiscInfoInMyCopmp

---
+++ DiscInfoInMyCopm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace DiscInfoInMyCopm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] dreivers = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in dreivers)
+            {
+                Console.WriteLine($"Name {drive.Name}");
+                Console.WriteLine($"Type {drive.DriveType}");
+
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Size {drive.TotalSize}");
+                    Console.WriteLine($"Free {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Metka {drive.VolumeLabel}");
+                }
+            }
+        }
+    }
+}
