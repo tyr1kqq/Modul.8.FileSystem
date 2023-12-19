904a64c8698e67ef312979acbc75629f566ea74e
tyr1kqq <igorturkinwhite@yandex.ru>
2023-12-19 12:58:34 +0200

    Create an Copy File

---
+++ NextMove/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+namespace NextMove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tempFile = Path.GetTempFileName();
+            var FileInfo = new FileInfo(tempFile);
+
+            using (StreamWriter sw = FileInfo.CreateText())
+            {
+                sw.WriteLine("Igor");
+                sw.WriteLine("Turkin");
+                sw.WriteLine("tyr1k_qq");
+
+            }
+
+            using (StreamReader sr = FileInfo.OpenText())
+            {
+            string str = " ";
+                while ((str = sr.ReadLine())!= null) 
+                {
+                    Console.WriteLine(str);
+                }
+            }
+            try
+            {
+                string tempFile2 = Path.GetTempFileName();
+                var FileInfo2 = new FileInfo(tempFile2);
+
+                FileInfo2.Delete();
+
+                FileInfo.CopyTo(tempFile2);
+                Console.WriteLine($"{tempFile} copy to {tempFile2}");
+                FileInfo.Delete();
+                Console.WriteLine($"{tempFile} has Deleted");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
