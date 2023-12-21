f9b0c669032247afbec74627784e329f0442239e
tyr1kqq <igorturkinwhite@yandex.ru>
2023-12-21 14:10:35 +0200

    End

---
+++ Binary/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+namespace Binary
+{
+    internal class Program
+    {
+        const string SettingsFileName = "Settings.cfg";
+        static void Main(string[] args)
+        {
+            WriteValues();
+
+            ReadValues();
+
+           NewFile();
+        }
+        static void NewFile()
+        {
+            string FilePath = "/Users/tyr1k_qq/Downloads/BinaryFile.bin";
+
+            if (File.Exists(FilePath))
+            {
+                string StringValue;
+
+                using (BinaryReader bin = new BinaryReader(File.Open(FilePath, FileMode.Open)))
+                {
+                    StringValue = bin.ReadString();
+                }
+
+                Console.WriteLine("Из файла считано");
+                Console.WriteLine(StringValue);
+            }
+        }
+
+        static void WriteValues()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(SettingsFileName , FileMode.Create)))
+            {
+                writer.Write(20.666f);
+                writer.Write(@"Text...");
+                writer.Write(55);
+                writer.Write(false);
+            }
+        }
+        static void ReadValues()
+        {
+            float FloatValue;
+            string StringValue;
+            int IntValue;
+            bool BoleanValue;
+            if (File.Exists(SettingsFileName))
+            {
+
+
+                using (BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
+                {
+                    FloatValue = reader.ReadSingle();
+                    StringValue = reader.ReadString();
+                    IntValue = reader.ReadInt32();
+                    BoleanValue = reader.ReadBoolean();
+                }
+
+                Console.WriteLine("Из файла считано:");
+
+                Console.WriteLine();
+
+                Console.WriteLine("Дробь:" + FloatValue);
+                Console.WriteLine("Строка:" + StringValue);
+                Console.WriteLine("Целое:" + IntValue);
+                Console.WriteLine("Булевое значение:" + BoleanValue);
+
+            }
+        }
+    }
+}
---
+++ Serializaple/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serializaple
+{
+    internal class Serializer
+    {
+        static void Main(string[] args)
+        {
+            var contact = new Contact("Igor", 89212636442, "@yandex.ru");
+
+            BinaryFormatter bf = new BinaryFormatter();
+            using (var formar = new FileStream("Contact.bin", FileMode.OpenOrCreate))
+            {
+                bf.Serialize(formar, contact);
+            }
+        }
+    }
+
+    [Serializable]
+    class Contact
+    {
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public  Contact(string name, long phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+
+    
+}
